34b68dc7a6c825517f3a7bea610a08952d7e0d53
Mirza Muhammad Ali Shahroze <ali.shahroze@confiz.com>
2025-01-27 02:50:02 +0500

    serve installed

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,18 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 3000,
-//     proxy: {
-//       '/api': {
-//         target: 'http://localhost:8000',
-//         changeOrigin: true,
-//         rewrite: (path) => path.replace(/^\/api/, ''),
-//       }
-//     }
-//   }
-// })
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './',
   server: {
-    port: process.env.PORT || 3000, // Use Heroku's dynamic port or fallback to 3000
+    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
-});
-
+      }
+    }
+  }
+})
