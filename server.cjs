6e8dcb8e176b86749adff09f605903c75d27d056
Mirza Muhammad Ali Shahroze <ali.shahroze@confiz.com>
2025-01-27 02:58:52 +0500

    serve installed

--- server.cjs
+++ server.cjs
@@ -1,10 +1,6 @@
-import express from 'express';
-import jsonServer from 'json-server';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require('express');
+const jsonServer = require('json-server');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8000;
