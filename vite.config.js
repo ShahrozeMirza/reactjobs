3613163ec795415cff9dc39130cb7178040063bf
Mirza Muhammad Ali Shahroze <ali.shahroze@confiz.com>
2025-01-27 01:38:12 +0500

    start script added

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,35 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:8000',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       }
+//     }
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   port: 3000,
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:8000',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //     }
-  //   }
-  // }
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: process.env.API_URL || 'http://localhost:8000',
+        target: process.env.BACKEND_URL || 'http://localhost:8000', // Use environment variable for API URL
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
   }
-  
 })
